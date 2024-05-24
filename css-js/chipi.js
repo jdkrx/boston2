760d7863a3b378271e600ccbd6e430383318ed8b
Santiago Santander <santiago.santander8765@gmail.com>
2024-05-24 12:27:34 -0400

    initial commit

---
+++ css-js/chipi.js
@@ -0,0 +1,20 @@
+let button2 = document.getElementById("button2");
+let content = document.getElementById("maincontent");
+let button = document.getElementById("button");
+let credits = document.getElementById("credits");
+
+button.onclick = () => {
+    credits.style.display = '';
+    credits.showModal();
+}
+
+function button2click() {
+    let pedro = new Audio();
+
+    pedro.src = './media/chipi/chipi.mp3';
+    pedro.play();
+    pedro.loop = true;
+    content.style.display = '';
+    button2.style.display = 'none';
+
+}
